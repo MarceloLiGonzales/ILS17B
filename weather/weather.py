2e756fab6d53dae42a3c9120b643800dd317f886
JohnVSantos <john.santos2@ucalgary.ca>
2021-04-05 15:08:04 -0600

    Added Code for the weather web server

---
+++ weather/weather.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import requests
+
+# Reference weather fetch code: https://github.com/KeithGalli/GUI
+# api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# a4aa5e3d83ffefaba8c00284de6ef7c3
+	
+def get_weather(city):
+
+        weather_key = 'a4aa5e3d83ffefaba8c00284de6ef7c3'
+        url_weather = 'https://api.openweathermap.org/data/2.5/weather'
+        params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+        response = requests.get(url_weather, params=params)
+        weather = response.json()
+        return weather
+
+app = Flask(__name__)
+
+@app.route('/<place>')
+
+def get_city(place):
+
+     weather_information =  get_weather(place)
+     environment=weather_information['weather'][0]['description']
+     temp = round((weather_information['main']['temp'] - 32) * (5/9), 1)
+     
+     return render_template('weather.html', the_city=place, the_environment=environment, the_temp=temp)
+      
+if __name__ == '__main__':
+        app.debug = True
+        app.run()
